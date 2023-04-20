a86ad6f6c316653e71d75bdb4e1130639181ce12
topmass <topmassproductions@gmail.com>
2023-04-20 08:54:11 -0700

    updated functionality and new gui

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -1,24 +1,27 @@
 import { useState } from 'react';
 import styles from './Chat.module.css';
-import {
-  Box,
-  Button,
-  Input,
-  Stack,
-  Text,
-} from '@chakra-ui/react';
+import { Box, Button, Input, Stack, Text } from '@chakra-ui/react';
 import { getGpt3Response } from '../../lib/gpt3';
 import React from 'react';
 
 function Option({ option, onClick }) {
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    onClick(option);
+    setClicked(true);
+    setTimeout(() => setClicked(false), 200);
+  };
+
   return (
     <Button
       size="sm"
       variant="outline"
       colorScheme="blue"
-      onClick={() => onClick(option)}
+      onClick={handleClick}
       marginRight="4px"
       marginBottom="4px"
+      className={clicked ? styles.optionClicked : ''}
     >
       {option}
     </Button>
@@ -29,66 +32,89 @@ function Chat() {
   const [chatHistory, setChatHistory] = useState([]);
   const [input, setInput] = useState('');
   const [options, setOptions] = useState([]);
+  const [thinking, setThinking] = useState(false);
+  const [fadeOut, setFadeOut] = useState(false);
+  const [hasMessageSent, setHasMessageSent] = useState(false);
 
-  async function handleSubmit(e) {
-    e.preventDefault();
-    setChatHistory([...chatHistory, { role: 'user', content: input }]);
+  async function handleSubmit(newChatHistory) {
+    setThinking(true);
+    setFadeOut(false);
     try {
       const response = await fetch('/api/gpt3', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ messages: [...chatHistory, { role: 'user', content: input }] }),
+        body: JSON.stringify({ messages: chatHistory.concat(newChatHistory) }),
       });
       const data = await response.json();
-      const newOptions = data.response.split('\n').filter(line => /^[a-zA-Z]\./.test(line));
-      setOptions(newOptions);
-      setChatHistory([
-        ...chatHistory,
-        { role: 'user', content: input },
-        { role: 'assistant', content: data.response.replace(/^[a-zA-Z]\..*\n?/gm, '') },
-      ]);
+      const responseObject = {
+        fullResponse: data.response,
+        options: data.response.split('\n').filter(line => /^[a-zA-Z]\./.test(line)),
+        message: data.response.replace(/^[a-zA-Z]\..*\n?/gm, ''),
+      };
+      setOptions(responseObject.options.length > 0 ? responseObject.options : []);
+      setChatHistory([...newChatHistory, { role: 'assistant', content: responseObject.message }]);
+      if (responseObject.options.length === 0) {
+        setInput('');
+      }
     } catch (error) {
       console.error('Error fetching GPT-3 response:', error);
     }
-    setInput('');
+    if (!hasMessageSent) {
+      setHasMessageSent(true);
+    } else {
+      setInput('');
+    }
+    setThinking(false);
+    setFadeOut(true);
   }
 
-  function handleOptionClick(option) {
-    setInput(option);
-    handleSubmit({ preventDefault: () => {} });
+  async function handleOptionClick(option) {
+    const newChatHistory = [...chatHistory, { role: 'user', content: option }];
+    setChatHistory(newChatHistory);
+    await handleSubmit(newChatHistory);
   }
 
   return (
     <Box className={styles.chatContainer}>
-      <Stack spacing={3}>
-        {chatHistory.map((msg, index) => (
-          <Text
-            key={index}
-            className={
-              msg.role === 'user'
-                ? styles.userMessage
-                : styles.assistantMessage
-            }
-          >
-            {msg.content}
-          </Text>
-        ))}
-        <Box display="flex" flexWrap="wrap">
-          {options.map((option, index) => (
-            <Option key={index} option={option} onClick={handleOptionClick} />
-          ))}
-        </Box>
-        <form onSubmit={handleSubmit}>
+      {hasMessageSent ? (
+        <>
+          {chatHistory.length > 0 &&
+            chatHistory.slice(-1).map((msg, index) => (
+              <Box key={index} className={styles.responseBox}>
+                <Text>{msg.content}</Text>
+                <Box className={styles.optionsContainer}>
+                  {options.map((option, index) => (
+                    <Option
+                      key={index}
+                      option={option}
+                      onClick={() => {
+                        handleOptionClick(option);
+                        setOptions([]);
+                      }}
+                    />
+                  ))}
+                </Box>
+              </Box>
+            ))}
+        </>
+      ) : null}
+      <Box className={`${styles.floatingInput} ${thinking || options.length > 0 ? styles.fadeOut : ''} ${
+          chatHistory.length === 0 ? styles.centeredInput : ''
+        }`}
+        onAnimationEnd={() => setFadeOut(false)}
+      >
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit([...chatHistory, { role: 'user', content: input }]);
+          }}
+        >
           <Stack direction="row" spacing={3}>
-            <Input
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-              placeholder="Type your message..."
-            />
+            <Input value={input} onChange={(e) => setInput(e.target.value)} placeholder="Type your message..." />
             <Button type="submit">Send</Button>
           </Stack>
         </form>
-      </Stack>
+      </Box>
     </Box>
   );
 }
--- lib/gpt3.ts
+++ lib/gpt3.ts
@@ -5,23 +5,22 @@ const GPT3_API_ENDPOINT = 'https://api.openai.com/v1/chat/completions';
 export async function getGpt3Response(messages) {
   const systemMessage = {
     role: 'system',
-    content:
-      'You are a helpful assistant. You are a building and permitting expert and your job is to help users know any and all permits or codes required to get the job done for their specific country and city. You answer as concise as possible. You will ask simple multiple choice questons in order to figure out the required codes and permits. You ask as little questions as possible. You always provide extra helpful tips and considerations once you are ready to provide the documentation for codes and permits.',
+    content: 'You are a helpful assistant and a building, construction and permitting expert. Your job is to gain information from the user to help the user find all permits, laws and codes required to get the job done for their specific Country and city. Assume they do not know anything about these requirements for their project. You answer as concise as possible. You will ask simple multiple choice questions in order to figure out the required codes, permits and laws. You ask as few questions as possible. You always provide links to permits and documentation directly as well as extra helpful tips and considerations once you are ready to provide the documentation for codes and permits. You only ever ask one question at a time. Each question you ask must have only one answer to select in order to answer it, no questions with two inputs. You never ask the user to type in the input; always provide multiple choice options for them to select.',
   };
 
-  messages = [systemMessage, ...messages];
-  
-  // Add the custom instruction to the first user message
-  if (messages.length > 1 && messages[1].role === 'user') {
-    messages[1].content =
-      "Reply with a multiple choice questions the options to choose from should each be on a new line and a lettered list for the user to reply to, continue ding this one question at a time until you are confident you know all the building prompts and codes required to complete the build project. You always format the answers to the questions specifically for the user to click one which will be sent back to you directly and following this format: A. Example reply 1 - then a new line - B. Example reply 2 - cont in that fashion " +
-      messages[1].content;
-  }
+  messages = messages.map((msg) => {
+    if (msg.role === 'user') {
+      return {
+        ...msg,
+        content: "User's response: " + msg.content + " Reply with a message that includes a brief response followed by multiple-choice questions. Format the options on a new line as a lettered list for the user to reply to. Continue asking questions one at a time until you are confident you know all the building prompts and codes required to complete the build project. Only ask questions that can be answered with a one-option click response. Format the answers to the questions specifically for the user to click one, which will be sent back to you directly. Follow this format: Brief response. A. Example reply 1 - then a new line - B. Example reply 2 - continue in that fashion.",
+      };
+    }
+    return msg;
+  });
 
-  const response = await axios.post(
-    GPT3_API_ENDPOINT,
-    { model: 'gpt-4', messages },
-    { headers: { 'Authorization': `Bearer ${OPENAI_API_KEY}` } }
-  );
+  messages = [systemMessage, ...messages];
+
+  const response = await axios.post(GPT3_API_ENDPOINT, { model: 'gpt-4', messages }, { headers: { 'Authorization': `Bearer ${OPENAI_API_KEY}` } });
+
   return response.data.choices[0].message.content;
 }
