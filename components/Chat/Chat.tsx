3a60de35a49a25409c8235ffc3672bc151a4beb2
matthewczege <matthewczegeny@gmail.com>
2023-04-21 20:34:50 -0700

    added geoloation feature

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -40,67 +40,54 @@ function isFinalResponse(response) {
   return !regex.test(response);
 }
 
+async function fetchLocation() {
+  const response = await fetch('https://ipapi.co/json/');
+  const data = await response.json();
+  return `${data.city}, ${data.country}`;
+}
+
 function Chat() {
   const [chatHistory, setChatHistory] = useState([]);
   const [input, setInput] = useState('');
   const [options, setOptions] = useState([]);
   const [thinking, setThinking] = useState(false);
   const [inputVisible, setInputVisible] = useState(true);
+  const [location, setLocation] = useState('');
+  const [changeLocationVisible, setChangeLocationVisible] = useState(false);
+  const [customLocation, setCustomLocation] = useState('');
 
   useEffect(() => {
     setInputVisible(chatHistory.length === 0);
+    fetchLocation().then((loc) => setLocation(loc));
   }, [chatHistory]);
 
   async function handleSubmit(newChatHistory) {
     setThinking(true);
     setInputVisible(false);
+
     const response = await fetch('/api/gpt3', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ messages: chatHistory.concat(newChatHistory) }),
     });
-    const data = await response.json();
 
+    const data = await response.json();
     const responseObject = {
       fullResponse: data.response,
-      options: data.response.split('\n').filter(line => /^[a-zA-Z]\./.test(line)),
+      options: data.response
+        .split('\n')
+        .filter((line) => /^[a-zA-Z]\./.test(line)),
       message: data.response.replace(/^[a-zA-Z]\..*\n?/gm, ''),
     };
 
     setOptions(responseObject.options.length > 0 ? responseObject.options : []);
-    setChatHistory([...newChatHistory, { role: 'assistant', content: responseObject.message }]);
+    setChatHistory([
+      ...newChatHistory,
+      { role: 'assistant', content: responseObject.message },
+    ]);
     setInput('');
     setInputVisible(true);
     setThinking(false);
-  }
-
-  interface OptionProps {
-    option: string;
-    onClick: (option: string) => void;
-  }
-
-  function Option({ option, onClick }: OptionProps) {
-    const [clicked, setClicked] = useState(false);
-
-    const handleClick = () => {
-      onClick(option);
-      setClicked(true);
-      setTimeout(() => setClicked(false), 200);
-    };
-
-    return (
-      <Button
-        size="sm"
-        variant="outline"
-        colorScheme="blue"
-        onClick={handleClick}
-        marginRight="4px"
-        marginBottom="4px"
-        className={clicked ? styles.optionClicked : ''}
-      >
-        {option}
-      </Button>
-    );
   }
 
   async function handleOptionClick(option) {
@@ -109,28 +96,47 @@ function Chat() {
     await handleSubmit(newChatHistory);
   }
 
+  async function handleLocationChange() {
+    setChangeLocationVisible(true);
+  }
+
+  async function handleCustomLocationSubmit(e) {
+    e.preventDefault();
+    setLocation(customLocation);
+    setChangeLocationVisible(false);
+  }
+
   return (
     <Box className={styles.chatContainer}>
       <Box>
-        {chatHistory.slice(-1).map((msg, index) => msg.role === 'assistant' && <AssistantMessage key={index} message={msg.content} />)}
+        {chatHistory.slice(-1).map((msg, index) => (
+          <AssistantMessage key={index} message={msg.content} />
+        ))}
       </Box>
       <Box
-        className={`${styles.floatingInput} ${chatHistory.length === 0 ? '' : styles.chatInputNotCentered}`}
+        className={`${styles.floatingInput} ${chatHistory.length === 0 ? '' : styles.chatInputNotCentered
+          }`}
         onAnimationEnd={() => setThinking(false)}
       >
         <form
-          onSubmit={e => {
+          onSubmit={(e) => {
             e.preventDefault();
-            handleSubmit([...chatHistory, { role: 'user', content: input }]);
+            handleSubmit([
+              ...chatHistory,
+              { role: 'user', content: `${input} in ${location}` },
+            ]);
             setThinking(true);
           }}
         >
-          <div className={styles.inputGlow} style={{ opacity: thinking ? 1 : 0 }}></div>
+          <div
+            className={styles.inputGlow}
+            style={{ opacity: thinking ? 1 : 0 }}
+          ></div>
           <Stack direction="row" spacing={3}>
             <Input
               className={styles.chatInput}
               value={input}
-              onChange={e => setInput(e.target.value)}
+              onChange={(e) => setInput(e.target.value)}
               placeholder="Type your message..."
               isDisabled={!inputVisible}
             />
@@ -139,13 +145,35 @@ function Chat() {
             </Button>
           </Stack>
         </form>
+        {chatHistory.length === 0 && (
+          <Box>
+            <Button onClick={handleLocationChange}>Change Location</Button>
+            {changeLocationVisible && (
+              <form onSubmit={handleCustomLocationSubmit}>
+                <Stack direction="row" spacing={3}>
+                  <Input
+                    className={styles.chatInput}
+                    value={customLocation}
+                    onChange={(e) => setCustomLocation(e.target.value)}
+                    placeholder="City, Country"
+                  />
+                  <Button type="submit">Update</Button>
+                </Stack>
+              </form>
+            )}
+          </Box>
+        )}
       </Box>
       <Box className={styles.optionsContainer}>
         {options.map((option, index) => (
-          <Option key={index} option={option} onClick={() => {
-            handleOptionClick(option);
-            setOptions([]);
-          }} />
+          <Option
+            key={index}
+            option={option}
+            onClick={() => {
+              handleOptionClick(option);
+              setOptions([]);
+            }}
+          />
         ))}
       </Box>
     </Box>
